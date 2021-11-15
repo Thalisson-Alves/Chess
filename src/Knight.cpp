ad5054db8030b78f671a94c56b0cbf59db9c304e
Thalisson-Alves <thalisson2030@gmail.com>
2021-11-15 13:16:05 -0300

    Add knight moves

--- src/Knight.cpp
+++ src/Knight.cpp
@@ -1,4 +1,5 @@
 #include "Knight.h"
+#include "Globals.h"
 
 Knight::Knight(const sf::Texture &texture, enum Piece::Type type, int position) :
         Piece(texture, type, position) {
@@ -6,5 +7,21 @@ Knight::Knight(const sf::Texture &texture, enum Piece::Type type, int position) 
 }
 
 std::vector<Piece::Move> Knight::getLegalMoves(const std::array<Piece::Ptr, 64> &pieces) const {
-    return std::vector<Piece::Move>();
+    std::vector<Piece::Move> moves;
+    std::vector<std::pair<int, int>> directions{{2,  1},
+                                                {1,  2},
+                                                {-1, 2},
+                                                {-2, 1},
+                                                {-2, -1},
+                                                {-1, -2},
+                                                {1,  -2},
+                                                {2,  -1}};
+    for (auto[xOffset, yOffset]: directions) {
+        int x = Position % 8 + xOffset, y = Position / 8 + yOffset;
+        if (Utils::isInBoardRange(x, y) &&
+            (pieces[x + 8 * y]->getType() == Piece::Type::None || isEnemy(pieces[x + 8 * y])))
+            moves.push_back({Position, x + 8 * y, Piece::Move::Type::Attack});
+    }
+
+    return moves;
 }
