49b5eea29aad4c65c7e570a610d01c50b64326a7
Thalisson-Alves <thalisson2030@gmail.com>
2021-11-15 13:11:27 -0300

    Add bishop moves

--- src/Bishop.cpp
+++ src/Bishop.cpp
@@ -4,5 +4,10 @@ Bishop::Bishop(const sf::Texture &texture, enum Piece::Type type, int position) 
         Piece(texture, type, position) {}
 
 std::vector<Piece::Move> Bishop::getLegalMoves(const std::array<Piece::Ptr, 64> &pieces) const {
-    return {};
+    std::vector<Piece::Move> moves;
+    std::vector<std::pair<int, int>> directions{{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+    for (auto [xOffset, yOffset] : directions)
+        pushMovesByDirection(pieces, moves, xOffset, yOffset);
+
+    return moves;
 }
