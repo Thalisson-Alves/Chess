ffcd480085bd15e792dc81b3a018e75f7ad3c78c
Thalisson-Alves <thalisson2030@gmail.com>
2021-11-15 13:09:45 -0300

    Add rook moves

--- src/Rook.cpp
+++ src/Rook.cpp
@@ -5,5 +5,12 @@ Rook::Rook(const sf::Texture &texture, enum Piece::Type type, int position) : Pi
 }
 
 std::vector<Piece::Move> Rook::getLegalMoves(const std::array<Piece::Ptr, 64> &pieces) const {
-    return std::vector<Piece::Move>();
+    auto moves = std::vector<Piece::Move>();
+    std::vector<std::pair<int, int>> directions{{1,  0},
+                                                {-1, 0},
+                                                {0,  1},
+                                                {0,  -1}};
+    for (auto[xOffset, yOffset]: directions)
+        pushMovesByDirection(pieces, moves, xOffset, yOffset);
+    return moves;
 }
